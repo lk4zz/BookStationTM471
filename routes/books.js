4dbff0a7897921c104cb5ab11e38bb0b83b6950d
unknown <you@example.com>
2026-03-10 17:43:10 +0200

    added genres and tagging to books

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const validateBook = require('../middlewares/validateBook.js');
-
+const bookGenreController = require('../controllers/bookGenreController');
 const bookController = require('../controllers/bookController');
 
 router.get('/', bookController.getAllBooks);
@@ -11,5 +11,6 @@ router.get('/:id', bookController.getBookById);
 router.get('/author/:authorId', bookController.getBooksByAuthor);
 router.put('/:id', verifyToken, validateBook, bookController.updateBook);
 router.delete('/:id', verifyToken, bookController.deleteBook);
+router.post('/:bookId/genres', verifyToken, bookGenreController.tagBook);
 
 module.exports = router;
